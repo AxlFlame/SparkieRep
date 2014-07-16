883bdceb87c2c0c326518e338f741067a7b54bb4
Yuri Guzon Mainka <yurigame@hotmail.com>
2014-07-16 17:14:50 -0300

    - Criação do prefab da gota de óleo mineral. - Criação do script de controle da vida da gota.

---
+++ Assets/Scripts/OilDropControl.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OilDropControl : MonoBehaviour {
+	public int fallSpeed;
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject, 0.9f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.position.y -= 1;
+	}
+}
