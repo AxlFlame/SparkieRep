aa97b15fb2a6362e43bfe23dd3115cdc4ee98ebf
Yuri Guzon Mainka <yurigame@hotmail.com>
2014-07-20 01:25:47 -0300

    Inclusão de scripts para controle do puzzle do final da primeira fase.

---
+++ Assets/Scripts/Level 01/DoorUnlock.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorUnlock : MonoBehaviour {
+	public GameObject bigDoor;
+	public GameObject puzzleBox;
+	public GameObject magnetizer;
+	public Vector3 doorPos;
+	public bool liftDoor;
+	// Use this for initialization
+	void Start () {
+		doorPos = bigDoor.GetComponent<Transform> ().position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		doorPos = bigDoor.GetComponent<Transform> ().position;
+		if (liftDoor)
+		{
+			doorPos.y += 0.05f;
+			bigDoor.GetComponent<Transform> ().position = doorPos;
+			if (doorPos.y >= -6)
+			{
+				doorPos.y = -6;
+				bigDoor.GetComponent<Transform> ().position = doorPos;
+				//liftDoor = false;
+			}
+		}
+	}
+
+	void OnCollisionEnter2D (Collision2D col)
+	{
+		if(col.gameObject.tag == "PuzzleBox")
+		{
+			magnetizer.GetComponent<MagnetizeSkill>().isConnectedToBox = false;
+			liftDoor = true;
+		}
+	}
+}
---
+++ Assets/Scripts/Level 01/PuzzleBoxController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PuzzleBoxController : MonoBehaviour {
+	public bool isEnabled = false;
+	private float sparkieX;
+	public GameObject player;
+	public Vector3 boxPos;
+	// Use this for initialization
+	void Start () {
+		boxPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isEnabled)
+		{
+			gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
+			sparkieX = player.GetComponent<Transform>().position.x;
+			boxPos.x = sparkieX;
+			boxPos.y = -8f;
+			transform.position = boxPos;
+		}
+		else
+		{
+			gameObject.GetComponent<Rigidbody2D>().gravityScale = 1;
+		}
+	}
+}
